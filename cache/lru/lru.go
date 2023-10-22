059ba092ea0a6d9955d87f3bfa1ccef37d72dec9
flybhjf <1091471517@qq.com>
2023-10-22 14:22:28 +0800

    1

---
+++ cache/lru/lru.go
@@ -0,0 +1,21 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes int64
+	nbytes   int64
+	ll       *list.List
+	cache    map[string]*list.Element
+
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
